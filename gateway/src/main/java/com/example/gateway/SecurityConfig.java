5406261b73c2b66ed03dbc02b9c3f8350c45ec8a
Katarina Tomasevic <tomasevickatarina152@gmail.com>
2025-11-03 22:33:10 +0100

    Added SecurityConfig for gateway, user and order services

---
+++ gateway/src/main/java/com/example/gateway/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/**").authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
